15dc3fac53324eea519d81d549da5f821a57e228
Dmitry Saks <dmitrysaks@gmail.com>
2022-12-12 15:15:54 -0800

    Develop

---
+++ BankApp.Application/DTOs/AccountDto.cs
@@ -0,0 +1,24 @@
+﻿using BankApp.Core.Domain.Entities;
+using BankApp.Core.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Application.DTOs
+{
+    public class AccountDto
+    {
+        public int AccountId { get; set; }
+        public string? AccountName { get; set; }
+        public string? AccountNumber { get; set; }
+        public string? TransitNumber { get; set; }
+        public string? BankNumber { get; set; }
+        public string? AbaRoutingNumber { get; set; }
+        public string? SwiftCode { get; set; }
+        public AccountType? AccountType { get; set; }
+        public ICollection<BankTransaction>? BankTransactions { get; set; } = new HashSet<BankTransaction>();
+        public ICollection<Balance>? Balances { get; set; } = new HashSet<Balance>();
+    }
+}
---
+++ BankApp/Profiles/AccountProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BankApp.Application.DTOs;
+using BankApp.Core.Domain.Entities;
+
+namespace BankApp.Profiles
+{
+    public class AccountProfile : Profile
+    {
+        public AccountProfile()
+        {
+            CreateMap<Account, AccountDto>().ReverseMap();
+        }
+    }
+}
